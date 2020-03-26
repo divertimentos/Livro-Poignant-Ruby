443739a612fb1867f4c8410437fcf43b92607b3e
guiemi <guiemi.teixeira@gmail.com>
2020-03-26 09:13:25 -0300

    Adds the notes file

---
+++ notas.rb
@@ -0,0 +1,36 @@
+# Blocos
+# 5.times {print "Sim, tenho usado bacon pedaçudo nos meus exemplos, mas não farei de novo! \n"}
+
+# As chaves podem ser substituídas pelas palavras DO e END,
+# o que é bacana se seu bloco for maior que uma linha
+
+# loop do
+#     print "Bem melhor!"
+#     print "Ah, mais espaço!"
+#     print "Minhas costas estavam me matando dentro daquelas garras de caranguejo."
+# end
+
+# require 'net/http'
+
+# Net::HTTP.start("www.ruby-lang.org", 80) do 
+#     |http| print(http.get('/en/about/LICENSE.txt').body)
+# end
+
+copo_plastico = nil
+
+# if copo_plastico
+#     print "O copo plástico está transbordando!"
+# end
+
+# unless copo_plastico
+#     print "O copo plástico está vazio!"
+# end
+
+# print "Yeah!, o copo plástico está cheio de novo!" if copo_plastico
+# print "\nDificilmente. Ele está vazio." unless copo_plastico
+
+# print "Estamos usando copos de plástio, pois não temos de vidro." if copo_plastico unless copo_vidro
+
+if nil.==(true).==(false)
+    print "Isto nunca vai ser printado!"
+end
