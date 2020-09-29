b8bd099be9c932069bf681ed0d3c83bbd99ba741
Guilherme Teixeira <guiemi.teixeira@gmail.com>
2020-09-29 18:56:21 -0300

    Adiciona notas novas e adiciona um gitignore com o idea dentro

--- notas.rb
+++ notas.rb
@@ -31,6 +31,41 @@ copo_plastico = nil
 
 # print "Estamos usando copos de plástio, pois não temos de vidro." if copo_plastico unless copo_vidro
 
-if nil.==(true).==(false)
-    print "Isto nunca vai ser printado!"
+# if nil.==(true).==(false)
+#     print "Isto nunca vai ser printado!"
+# end
+
+# no_hotel = true
+# print(
+#     if no_hotel.nil?
+#         "Sem pistas se ele está no hotel!"
+#     elsif no_hotel == true
+#         "Definitivamente sim!"
+#     elsif no_hotel == false
+#         "Ele saiu!"
+#     else
+#         "O sistema está travaaaaaad-iz"
+#     end)
+
+# print "Digite e seja diabólico: \n"
+# ideia_contrario = gets.reverse
+# print ideia_contrario
+
+palavras_codigo = {
+    'macacosestrela' => 'Phil e Pete, aqueles chanceleres de pavio curto do Novo Reich', 
+    'catapulta' => 'chucky go-go', 
+    'bomba' => 'Vida Assistida por Calor',
+    'Nigeria' => "Ny e Jerry's Lavagem a Seco (com Donuts)",
+    'Ponha um fim nisso' => 'Ponha um fio nisso'
+    }
+
+print "Entre com sua nova ideia: \n"
+ideia = gets
+palavras_codigo.each do |real, codigo|
+    ideia.gsub!(real, codigo)
 end
+
+print "Arquivo codificado. Favor entrar com um nome para essa ideia: \n"
+nome_ideia = gets.strip
+File::open("ideia-" + nome_ideia + ".txt", "w") do |f|
+    f << ideia
---
+++ teste.rb
@@ -0,0 +1,10 @@
+no_hotel = true
+
+email = if no_hotel
+            endereco = "guiemi"
+            endereco << "@gmail"
+            endereco << ".com"
+end
+
+puts email
+
